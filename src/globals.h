f361c9dfb6e2ba3d5e10591292de068356f540aa
gpnuma <guillaume@centaurean.com>
2015-03-27 02:05:10 +0100

    Moved binary to uint macro in global header

--- src/globals.h
+++ src/globals.h
@@ -69,6 +69,29 @@
 #error Unknow endianness
 #endif
 
+#define DENSITY_FORMAT(v)               0##v##llu
+
+#define DENSITY_ISOLATE(b, p)           ((DENSITY_FORMAT(b) / p) & 0x1)
+
+#define DENSITY_BINARY_TO_UINT(b)        ((DENSITY_ISOLATE(b, 1llu) ? 0x1 : 0)\
+                                        + (DENSITY_ISOLATE(b, 8llu) ? 0x2 : 0)\
+                                        + (DENSITY_ISOLATE(b, 64llu) ? 0x4 : 0)\
+                                        + (DENSITY_ISOLATE(b, 512llu) ? 0x8 : 0)\
+                                        + (DENSITY_ISOLATE(b, 4096llu) ? 0x10 : 0)\
+                                        + (DENSITY_ISOLATE(b, 32768llu) ? 0x20 : 0)\
+                                        + (DENSITY_ISOLATE(b, 262144llu) ? 0x40 : 0)\
+                                        + (DENSITY_ISOLATE(b, 2097152llu) ? 0x80 : 0)\
+                                        + (DENSITY_ISOLATE(b, 16777216llu) ? 0x100 : 0)\
+                                        + (DENSITY_ISOLATE(b, 134217728llu) ? 0x200 : 0)\
+                                        + (DENSITY_ISOLATE(b, 1073741824llu) ? 0x400 : 0)\
+                                        + (DENSITY_ISOLATE(b, 8589934592llu) ? 0x800 : 0)\
+                                        + (DENSITY_ISOLATE(b, 68719476736llu) ? 0x1000 : 0)\
+                                        + (DENSITY_ISOLATE(b, 549755813888llu) ? 0x2000 : 0)\
+                                        + (DENSITY_ISOLATE(b, 4398046511104llu) ? 0x4000 : 0)\
+                                        + (DENSITY_ISOLATE(b, 35184372088832llu) ? 0x8000 : 0)\
+                                        + (DENSITY_ISOLATE(b, 281474976710656llu) ? 0x10000 : 0)\
+                                        + (DENSITY_ISOLATE(b, 2251799813685248llu) ? 0x20000 : 0))
+
 #define DENSITY_UNROLL_2(op)     op; op
 #define DENSITY_UNROLL_4(op)     DENSITY_UNROLL_2(op);    DENSITY_UNROLL_2(op)
 #define DENSITY_UNROLL_8(op)     DENSITY_UNROLL_4(op);    DENSITY_UNROLL_4(op)
--- src/kernel_lion.h
+++ src/kernel_lion.h
@@ -59,29 +59,6 @@
 
 #define DENSITY_LION_BIGRAM_HASH_ALGORITHM(bigram)                      (uint8_t) ((((bigram) * DENSITY_LION_HASH32_MULTIPLIER) >> (32 - DENSITY_LION_BIGRAM_HASH_BITS)))
 
-#define DENSITY_FORMAT(v)               0##v##llu
-
-#define DENSITY_ISOLATE(b, p)           ((DENSITY_FORMAT(b) / p) & 0x1)
-
-#define DENSITY_BINARY_TO_UINT(b)        ((DENSITY_ISOLATE(b, 1llu) ? 0x1 : 0)\
-                                        + (DENSITY_ISOLATE(b, 8llu) ? 0x2 : 0)\
-                                        + (DENSITY_ISOLATE(b, 64llu) ? 0x4 : 0)\
-                                        + (DENSITY_ISOLATE(b, 512llu) ? 0x8 : 0)\
-                                        + (DENSITY_ISOLATE(b, 4096llu) ? 0x10 : 0)\
-                                        + (DENSITY_ISOLATE(b, 32768llu) ? 0x20 : 0)\
-                                        + (DENSITY_ISOLATE(b, 262144llu) ? 0x40 : 0)\
-                                        + (DENSITY_ISOLATE(b, 2097152llu) ? 0x80 : 0)\
-                                        + (DENSITY_ISOLATE(b, 16777216llu) ? 0x100 : 0)\
-                                        + (DENSITY_ISOLATE(b, 134217728llu) ? 0x200 : 0)\
-                                        + (DENSITY_ISOLATE(b, 1073741824llu) ? 0x400 : 0)\
-                                        + (DENSITY_ISOLATE(b, 8589934592llu) ? 0x800 : 0)\
-                                        + (DENSITY_ISOLATE(b, 68719476736llu) ? 0x1000 : 0)\
-                                        + (DENSITY_ISOLATE(b, 549755813888llu) ? 0x2000 : 0)\
-                                        + (DENSITY_ISOLATE(b, 4398046511104llu) ? 0x4000 : 0)\
-                                        + (DENSITY_ISOLATE(b, 35184372088832llu) ? 0x8000 : 0)\
-                                        + (DENSITY_ISOLATE(b, 281474976710656llu) ? 0x10000 : 0)\
-                                        + (DENSITY_ISOLATE(b, 2251799813685248llu) ? 0x20000 : 0))
-
 typedef enum {
     DENSITY_LION_FORM_CHUNK_PREDICTIONS,
     DENSITY_LION_FORM_CHUNK_SECONDARY_PREDICTIONS,
