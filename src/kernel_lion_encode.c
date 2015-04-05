c168fc27c6a3a4fea2adb26d6eedda3c9ec654f3
gpnuma <guillaume@centaurean.com>
2015-04-05 21:53:02 +0200

    Added utility method to push entropy codes to signature

--- src/kernel_lion_encode.c
+++ src/kernel_lion_encode.c
@@ -131,6 +131,11 @@ DENSITY_FORCE_INLINE void density_lion_encode_push_zero_to_signature(density_mem
     }
 }
 
+DENSITY_FORCE_INLINE void density_lion_encode_push_code_to_signature(density_memory_location *restrict out, density_lion_encode_state *restrict state, const density_lion_entropy_code code) {
+    density_lion_encode_push_to_signature(out, state, code.value, code.bitLength);
+}
+
+
 DENSITY_FORCE_INLINE void density_lion_encode_manage_bigram(density_memory_location *restrict out, density_lion_encode_state *restrict state, const uint8_t hash, const uint16_t bigram) {
     density_lion_dictionary_bigram_entry *bigram_entry = &state->dictionary.bigrams[hash];
     if (bigram_entry->bigram != bigram) {
@@ -165,15 +170,13 @@ DENSITY_FORCE_INLINE void density_lion_encode_kernel(density_memory_location *re
             if (*found_a ^ chunk) {
                 uint32_t *found_b = &found->chunk_b;
                 if (*found_b ^ chunk) {
-                    const density_lion_entropy_code codeSA = density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_SECONDARY_ACCESS);
-                    density_lion_encode_push_to_signature(out, state, codeSA.value, codeSA.bitLength);
+                    density_lion_encode_push_code_to_signature(out, state, density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_SECONDARY_ACCESS));
 
                     const uint32_t hash_group = (uint16_t)((uint16_t)chunk * DENSITY_LION_HASH16_MULTIPLIER) | ((chunk & DENSITY_MASK_16_32) * DENSITY_LION_HASH16_MULTIPLIER);
                     density_lion_encode_manage_bigram(out, state, *((uint8_t *) &hash_group + 1), (uint16_t)chunk);
                     density_lion_encode_manage_bigram(out, state, *((uint8_t *) &hash_group + 3), *((uint16_t *) &chunk + 1));
                 } else {
-                    const density_lion_entropy_code codeDB = density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_CHUNK_DICTIONARY_B);
-                    density_lion_encode_push_to_signature(out, state, codeDB.value, codeDB.bitLength);
+                    density_lion_encode_push_code_to_signature(out, state, density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_CHUNK_DICTIONARY_B));
 
                     *(uint16_t *) (out->pointer) = DENSITY_LITTLE_ENDIAN_16(hash);
                     out->pointer += sizeof(uint16_t);
@@ -181,8 +184,7 @@ DENSITY_FORCE_INLINE void density_lion_encode_kernel(density_memory_location *re
                 *found_b = *found_a;
                 *found_a = chunk;
             } else {
-                const density_lion_entropy_code codeDA = density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_CHUNK_DICTIONARY_A);
-                density_lion_encode_push_to_signature(out, state, codeDA.value, codeDA.bitLength);
+                density_lion_encode_push_code_to_signature(out, state, density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_CHUNK_DICTIONARY_A));
 
                 *(uint16_t *) (out->pointer) = DENSITY_LITTLE_ENDIAN_16(hash);
                 out->pointer += sizeof(uint16_t);
@@ -192,8 +194,7 @@ DENSITY_FORCE_INLINE void density_lion_encode_kernel(density_memory_location *re
         *(uint64_t *) ((uint32_t *) p + 1) = *(uint64_t *) p;
         p->next_chunk_a = chunk;    // Move chunk to the top of the predictions list
     } else {
-        const density_lion_entropy_code codePa = density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_CHUNK_PREDICTIONS);
-        density_lion_encode_push_to_signature(out, state, codePa.value, codePa.bitLength);
+        density_lion_encode_push_code_to_signature(out, state, density_lion_form_model_get_encoding(&state->formData, DENSITY_LION_FORM_CHUNK_PREDICTIONS));
     }
 
     state->lastHash = hash;
