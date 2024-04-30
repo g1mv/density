33e0283d7a72e4838d17d6b63b5e6f1d9f42ab29
Guillaume <3772944+g1mv@users.noreply.github.com>
2024-04-30 13:36:55 +0200

    Frames trial

--- src/algorithms/chameleon_writer.rs
+++ src/algorithms/chameleon_writer.rs
@@ -2,7 +2,10 @@ use std::io::Result;
 use std::io::Write;
 use std::ops::Index;
 
-use crate::algorithms::chameleon::{BYTE_SIZE_U128, BYTE_SIZE_U16, BYTE_SIZE_U32, BYTE_SIZE_U64, CHAMELEON_HASH_BITS, CHAMELEON_HASH_MULTIPLIER, Signature};
+use crate::algorithms::chameleon::{BYTE_SIZE_U128, BYTE_SIZE_U32, BYTE_SIZE_U64, CHAMELEON_HASH_BITS, CHAMELEON_HASH_MULTIPLIER, Signature};
+
+const BYTE_SIZE_FRAME: usize = BYTE_SIZE_U64 + 64 * BYTE_SIZE_U32;
+const NUM_SUBFRAMES: usize = 1;
 
 struct Slice<'a, T: 'a>(&'a [T], &'a [T]);
 
@@ -17,26 +20,39 @@ impl<'a, T: 'a> Index<usize> for Slice<'a, T> {
     }
 }
 
-pub struct ChameleonWriter<'a> {
-    pub buffer: &'a mut [u8],
-    pub dictionary: [u32; 1 << CHAMELEON_HASH_BITS],
-    pub signature: Signature,
-    pub sgn_index: usize,
-    pub out_index: usize,
-    // pub temp_buffer: [u8; BYTE_SIZE_U128],
-    // pub temp_size: usize,
+pub struct Sig128 {
+    pub(crate) value: u128,
+    pub(crate) shift: u8,
 }
 
-impl<'a> ChameleonWriter<'a> {
-    pub fn new(buffer: &'a mut [u8]) -> Self {
+pub struct ChameleonWriter<W: Write> {
+    pub writer: W,
+    pub dictionary: [u32; 1 << CHAMELEON_HASH_BITS],
+    pub signature: Signature,
+    // pub sgn_index: usize,
+    // pub out_index: usize,
+    // pub temp_buffer: [u8; BYTE_SIZE_U128],
+    // pub temp_size: usize,
+    pub frame: [u8; NUM_SUBFRAMES * BYTE_SIZE_FRAME],
+    pub sgn_index: usize,
+    pub frm_index: usize,
+    pub frm_num: usize,
+}
+
+impl<W: Write> ChameleonWriter<W> {
+    pub fn new(writer: W) -> Self {
         ChameleonWriter {
-            buffer,
+            writer,
             dictionary: [0; 1 << CHAMELEON_HASH_BITS],
             signature: Signature { value: 0, shift: 0 },
-            sgn_index: 0,
-            out_index: BYTE_SIZE_U64,
+            // sgn_index: 0,
+            // out_index: BYTE_SIZE_U64,
             // temp_buffer: [0; BYTE_SIZE_U128],
             // temp_size: 0,
+            frame: [0; NUM_SUBFRAMES * BYTE_SIZE_FRAME],
+            frm_index: BYTE_SIZE_U64,
+            sgn_index: 0,
+            frm_num: 0,
         }
     }
 
@@ -58,35 +74,64 @@ impl<'a> ChameleonWriter<'a> {
     }
 
     #[inline(always)]
+    pub fn push_to_frame(&mut self, bytes: &[u8]) {
+        self.frame[self.frm_index..self.frm_index + bytes.len()].copy_from_slice(&bytes);
+        self.frm_index += bytes.len();
+    }
+
+    #[inline(always)]
+    pub fn complete_frame(&mut self) -> bool {
+        self.frame[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
+        if self.frm_num >= NUM_SUBFRAMES - 1 {
+            self.frm_num = 0;
+            self.sgn_index = 0;
+            self.frm_index = BYTE_SIZE_U64;
+            true
+        } else {
+            self.sgn_index = self.frm_index;
+            self.frm_num += 1;
+            false
+        }
+    }
+
+    #[inline(always)]
     pub fn kernel(&mut self, value_u32: u32) {
         let hash_u16 = ((value_u32 * CHAMELEON_HASH_MULTIPLIER) >> (32 - CHAMELEON_HASH_BITS)) as u16;
         let dictionary_value = &mut self.dictionary[hash_u16 as usize];
         if &value_u32 == dictionary_value {
             if self.push_sgn_bit(1) {
-                self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
-                self.sgn_index = self.out_index;
-                self.out_index += BYTE_SIZE_U64;
+                if self.complete_frame() {
+                    self.writer.write(&self.frame[0..self.frm_index]).expect("ouch");
+                }
+                // self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
+                // self.sgn_index = self.out_index;
+                // self.out_index += BYTE_SIZE_U64;
             }
-            self.buffer[self.out_index..self.out_index + BYTE_SIZE_U16].copy_from_slice(&hash_u16.to_le_bytes());
-            self.out_index += BYTE_SIZE_U16;
+            self.push_to_frame(&hash_u16.to_le_bytes());
+            // self.buffer[self.out_index..self.out_index + BYTE_SIZE_U16].copy_from_slice(&hash_u16.to_le_bytes());
+            // self.out_index += BYTE_SIZE_U16;
         } else {
             *dictionary_value = value_u32;
             if self.push_sgn_bit(0) {
-                self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
-                self.sgn_index = self.out_index;
-                self.out_index += BYTE_SIZE_U64;
+                if self.complete_frame() {
+                    self.writer.write(&self.frame[0..self.frm_index]).expect("ouch");
+                }
+                // self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
+                // self.sgn_index = self.out_index;
+                // self.out_index += BYTE_SIZE_U64;
             }
-            self.buffer[self.out_index..self.out_index + BYTE_SIZE_U32].copy_from_slice(&value_u32.to_le_bytes());
-            self.out_index += BYTE_SIZE_U32;
+            self.push_to_frame(&value_u32.to_le_bytes());
+            // self.buffer[self.out_index..self.out_index + BYTE_SIZE_U32].copy_from_slice(&value_u32.to_le_bytes());
+            // self.out_index += BYTE_SIZE_U32;
         }
     }
 }
 
-impl Write for ChameleonWriter<'_> {
+impl<W: Write> Write for ChameleonWriter<W> {
     fn write(&mut self, input: &[u8]) -> Result<usize> {
         // println!("{}", input.len());
 
-        let start_index = 0;
+        // let start_index = 0;
         // let mut shift_index = 0;    // BYTE_SIZE_U128 - self.temp_size;
 
         // if self.temp_size != 0 {
@@ -163,14 +208,14 @@ impl Write for ChameleonWriter<'_> {
             }
         }
 
-        self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
+        // self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
 
-        println!("{} bytes", self.out_index - start_index);
+        // println!("{} bytes", self.out_index - start_index);
         Ok(input.len())
     }
 
     fn flush(&mut self) -> Result<()> {
-        self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
+        // self.buffer[self.sgn_index..self.sgn_index + BYTE_SIZE_U64].copy_from_slice(&self.signature.value.to_le_bytes());
         // self.final_index = self.out_index;
         Ok(())
     }
