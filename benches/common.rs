04c7355c9ba34c4e6bc2e2ee60829ebd3f6fa929
Guillaume <3772944+g1mv@users.noreply.github.com>
2024-08-24 14:58:22 +0200

    Architecture progress

--- benches/common.rs
+++ benches/common.rs
@@ -8,18 +8,18 @@ pub const DATA_DIRECTORY: &str = "benches/data/";
 pub const TEST_FILE: &str = "enwik8";
 
 pub fn prepare_file() {
-    let silesia_tar_path = &format!("{}{}", DATA_DIRECTORY, TEST_FILE);
+    let test_file_path = &format!("{}{}", DATA_DIRECTORY, TEST_FILE);
 
-    if !Path::new(silesia_tar_path).exists() {
-        let in_mem = read(&format!("{}.xz", silesia_tar_path)).unwrap();
-        println!("Decompressing {}.xz ({} bytes)...", silesia_tar_path, in_mem.len());
+    if !Path::new(test_file_path).exists() {
+        let in_mem = read(&format!("{}.xz", test_file_path)).unwrap();
+        println!("Decompressing {}.xz ({} bytes)...", test_file_path, in_mem.len());
         let mut decoder = XzDecoder::new(in_mem.as_slice());
         let mut out_mem = Vec::with_capacity(in_mem.len() << 3);
         decoder.read_to_end(&mut out_mem).unwrap();
-        println!("Decompressed to {} ({} bytes)", silesia_tar_path, out_mem.len());
-        fs::write(&format!("{}", silesia_tar_path), out_mem).unwrap();
+        println!("Decompressed to {} ({} bytes)", test_file_path, out_mem.len());
+        fs::write(&format!("{}", test_file_path), out_mem).unwrap();
     }
 
-    let in_mem = read(silesia_tar_path).unwrap();
-    println!("Using file {} ({} bytes)", silesia_tar_path, in_mem.len());
+    let in_mem = read(test_file_path).unwrap();
+    println!("Using file {} ({} bytes)", test_file_path, in_mem.len());
 }
